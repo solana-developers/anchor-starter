8814cf27c4b43fbcc44ef0bea5c9d9f3efdd2a24
John <johnzhengyuliu@gmail.com>
2023-08-29 00:27:31 -0500

    delete comments

--- 02.Anchor/00.hello_anchor/programs/hello_anchor/src/lib.rs
+++ 02.Anchor/00.hello_anchor/programs/hello_anchor/src/lib.rs
@@ -1,26 +1,19 @@
 use anchor_lang::prelude::*;
 
-// This is your program's public key
 declare_id!("7UMEGk3CQ1P4fg7xHm1ZnKGZWP1jT3XobP3oiW1JRtB8");
 
-// Define the program instructions
 #[program]
 mod hello_anchor {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
         ctx.accounts.new_account.data = data;
-        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
+        msg!("Changed data to: {}!", data);
         Ok(())
     }
 }
 
-// Define the accounts required by the program instruction
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // We must specify the space in order to initialize an account.
-    // First 8 bytes are default account discriminator,
-    // next 8 bytes come from NewAccount.data being type u64.
-    // (u64 = 64 bits unsigned integer = 8 bytes)
     #[account(init, payer = signer, space = 8 + 8)]
     pub new_account: Account<'info, NewAccount>,
     #[account(mut)]
@@ -28,7 +21,6 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// Define the layout of the NewAccount account.
 #[account]
 pub struct NewAccount {
     data: u64,
--- 02.Anchor/00.hello_anchor/tests/hello_anchor.ts
+++ 02.Anchor/00.hello_anchor/tests/hello_anchor.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { HelloAnchor } from "../target/types/hello_anchor";
 import { Keypair, SystemProgram } from "@solana/web3.js";
 
@@ -11,15 +11,10 @@ describe("hello_anchor", () => {
   const program = anchor.workspace.HelloAnchor as Program<HelloAnchor>;
 
   it("Is initialized!", async () => {
-    // Generate keypair for the new account
     const newAccountKeypair = new Keypair();
 
-    // Instruction data
-    const data = new anchor.BN(42);
-
-    // Send transaction
     const transactionSignature = await program.methods
-      .initialize(data)
+      .initialize(new BN(42))
       .accounts({
         newAccount: newAccountKeypair.publicKey,
         signer: wallet.publicKey,
