db91e394e11c8129505f7b81bec0f7f380344e01
John <johnzhengyuliu@gmail.com>
2023-10-09 11:09:54 -0500

    remove unnecessary wallet-adapters (backpack/phantom)

--- 04.frontend/contexts/WalletContextProvider.tsx
+++ 04.frontend/contexts/WalletContextProvider.tsx
@@ -1,30 +1,20 @@
-import { FC, ReactNode, useMemo } from "react"
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
   ConnectionProvider,
   WalletProvider,
-} from "@solana/wallet-adapter-react"
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  BackpackWalletAdapter,
-} from "@solana/wallet-adapter-wallets"
-import { clusterApiUrl } from "@solana/web3.js"
-require("@solana/wallet-adapter-react-ui/styles.css")
+} from "@solana/wallet-adapter-react";
+import { FC, ReactNode, useMemo } from "react";
+
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const network = WalletAdapterNetwork.Devnet
-  const endpoint = useMemo(() => clusterApiUrl(network), [network])
-
-  const wallets = useMemo(
-    () => [
-      new BackpackWalletAdapter(),
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
-    [network]
-  )
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new SolflareWalletAdapter()], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
@@ -32,7 +22,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
-  )
-}
+  );
+};
 
-export default WalletContextProvider
+export default WalletContextProvider;
